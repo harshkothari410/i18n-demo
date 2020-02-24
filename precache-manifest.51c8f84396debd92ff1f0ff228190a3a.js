ff0ee241cddb1babfc1dbae5f0e452d223b2b736
Harsh Kothari <harshkothari410@gmail.com>
2020-02-24 13:33:21 -0800

    Updates

---
+++ precache-manifest.51c8f84396debd92ff1f0ff228190a3a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eac74fe839e74dccdf5cf0e8ef379424",
+    "url": "/i18n-demo/index.html"
+  },
+  {
+    "revision": "749c4997f8f70bd5cf9b",
+    "url": "/i18n-demo/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "c3e978c9527db1b2a0fd",
+    "url": "/i18n-demo/static/css/main.6a934dfc.chunk.css"
+  },
+  {
+    "revision": "749c4997f8f70bd5cf9b",
+    "url": "/i18n-demo/static/js/2.2c3479c0.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/i18n-demo/static/js/2.2c3479c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c3e978c9527db1b2a0fd",
+    "url": "/i18n-demo/static/js/main.32a7ffcf.chunk.js"
+  },
+  {
+    "revision": "651127edd58582779669",
+    "url": "/i18n-demo/static/js/runtime-main.5e6f908a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/i18n-demo/static/media/logo.5d5d9eef.svg"
+  }
+]);
